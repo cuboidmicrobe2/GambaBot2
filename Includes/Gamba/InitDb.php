9205e010a64486c01ac9f5760e3360f4879c022e
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-13 23:54:31 +0200

    PDO\mysql

--- Includes/Gamba/InitDb.php
+++ Includes/Gamba/InitDb.php
@@ -1,6 +1,6 @@
 <?php
 
-new PDO('mysql:host='.$_ENV['DB_HOSTNAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'])->query(<<<SQL
+PDO::connect('mysql:host='.$_ENV['DB_HOSTNAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'])->query(<<<SQL
     CREATE DATABASE IF NOT EXISTS gamba;
     CREATE DATABASE IF NOT EXISTS gamba_inventories;
     USE gamba;
