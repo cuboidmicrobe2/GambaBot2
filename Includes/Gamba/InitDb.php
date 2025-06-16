47e26ae2d90d849e17452ee4e298e53ea7a3cbef
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-16 20:49:25 +0200

    coin_inventory to user_stats + pity system and PDO::ERRMODE_EXCEPTION

--- Includes/Gamba/InitDb.php
+++ Includes/Gamba/InitDb.php
@@ -1,6 +1,8 @@
 <?php
 
-PDO::connect('mysql:host='.$_ENV['DB_HOSTNAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'])->query(<<<SQL
+$tempDB = PDO::connect('mysql:host='.$_ENV['DB_HOSTNAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+$tempDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$tempDB->query(<<<SQL
     CREATE DATABASE IF NOT EXISTS gamba;
     CREATE DATABASE IF NOT EXISTS gamba_inventories;
     USE gamba;
@@ -13,8 +15,11 @@ PDO::connect('mysql:host='.$_ENV['DB_HOSTNAME'], $_ENV['DB_USERNAME'], $_ENV['DB
 
     USE gamba_inventories;
 
-    CREATE TABLE IF NOT EXISTS coin_inventory (
+    CREATE TABLE IF NOT EXISTS user_stats (
         uid BIGINT UNSIGNED PRIMARY KEY NOT NULL,
-        coins BIGINT UNSIGNED
+        coins BIGINT UNSIGNED DEFAULT 0,
+        purple_pity SMALLINT UNSIGNED DEFAULT 0,
+        gold_pity SMALLINT UNSIGNED DEFAULT 0
     );
 SQL);
+unset($tempDB);
