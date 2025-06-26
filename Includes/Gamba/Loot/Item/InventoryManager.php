0be9a164282763df2967cd56e461959cfb43b67a
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-26 16:46:17 +0200

    new function InventoryManager::leaderboard()

--- Includes/Gamba/Loot/Item/InventoryManager.php
+++ Includes/Gamba/Loot/Item/InventoryManager.php
@@ -2,8 +2,11 @@
 
 namespace Gamba\Loot\Item;
 
+use CurlHandle;
 use Gamba\Loot\Item\Inventory;
+use HTTP\Request;
 use Pdo\Mysql;
+use OutOfRangeException;
 
 class InventoryManager { // mby dont need this (needs something to store pdo obj, so mby this?)
 
@@ -16,5 +19,42 @@ class InventoryManager { // mby dont need this (needs something to store pdo obj
 
     public function getInventory(string $uid) : Inventory {
         return new Inventory($uid, $this->conn);
+    }
+
+    public function leaderboard(int $top) : array {
+        if($top < 1) throw new OutOfRangeException('leaderboard $top cannot be less than 1');
+
+        $result = $this->conn->query(<<<SQL
+            SELECT uid, coins
+            FROM user_stats
+            ORDER BY coins DESC
+            LIMIT {$top};
+        SQL);
+
+        $data = [];
+
+        $requests = new Request();
+        while($row = $result->fetch(Mysql::FETCH_ASSOC)) {
+            $data[]['coins'] = $row['coins'];
+
+            $requests->bind('https://discord.com/api/v9/users/' . $row['uid'], [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                    'Authorization: Bot ' . $_ENV['DISCORD_TOKEN']
+                ]
+            ]);
+        }
+
+        $requests->execute();
+
+        $i = 0;
+        foreach($requests->fetch() as $userData) {
+            $userInfo = json_decode($userData, true);
+            $name = isset($userInfo['global_name']) ? $userInfo['global_name'] : (isset($userInfo['username']) ? $userInfo['username'] : 'CURL_ERROR');
+            $data[$i]['user'] = $name;
+            $i++;
+        }
+
+        return $data;
     }
 }
