332e3b892a6966b876eb61556c737a19876482b6
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-26 16:45:04 +0200

    new leaderboard command

---
+++ Commands/leaderboard.php
@@ -0,0 +1,25 @@
+<?php
+
+use Discord\Builders\MessageBuilder;
+use Discord\Parts\Embed\Embed;
+use Discord\Parts\Interactions\Interaction;
+
+global $discord, $gamba;
+
+$discord->listenCommand('leaderboard', function(Interaction $interaction) use ($gamba, $discord) {
+    $interaction->acknowledgeWithResponse()->then(function() use ($interaction, $gamba, $discord) {
+        $leaderboard = $gamba->inventoryManager->leaderboard(10);
+
+        
+        $text = '';
+        for($i = 0; $i < count($leaderboard); $i++) {
+            $text .= $leaderboard[$i]['user'] . ' - ' . $leaderboard[$i]['coins'] . PHP_EOL;
+        }
+
+        $interaction->updateOriginalResponse(MessageBuilder::new()->addEmbed(new Embed($discord)
+            ->setTitle('Leaderboard')
+            ->setDescription($text)
+            ->setColor(EMBED_COLOR_PINK)
+        ));
+    });
+});
