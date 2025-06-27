f718361f19e22d22823688000d0fbca4908c326f
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-27 14:30:56 +0200

    creates resources needed to run the bot

---
+++ Setup.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+set dir=%~dp0
+
+call :Main
+goto :EOF
+
+:Main
+    echo Make sure that the XAMPP database in online before running the setup
+    pause
+    cls
+    call :CreateShortcut
+    call :CreateDatabase
+    pause
+goto :EOF
+
+
+
+:CreateShortcut
+    echo Creating desktop shortcut...
+    set TARGET=%dir%GambaBot.bat
+    set SHORTCUT=%USERPROFILE%\Desktop\GambaBot.lnk
+    set ICON=%dir%icon.ico
+    set arg=/l
+    set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
+
+    %PWS% -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(\"%SHORTCUT%\"); $Shortcut.TargetPath = \"%TARGET%\"; $Shortcut.Arguments = \"%arg%\"; $Shortcut.IconLocation = \"%ICON%\"; $Shortcut.Save()"
+    echo Shortcut created!
+goto :EOF
+
+:CreateDatabase
+    echo Creating database...
+    php %dir%\Includes\Gamba\InitDb.php
+    echo Database has been created!
+pause
