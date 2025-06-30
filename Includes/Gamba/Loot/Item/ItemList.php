5d89cf41637b881d8bbcf85b2311217f4603210d
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-30 21:55:41 +0200

    added some new items

--- Includes/Gamba/Loot/Item/ItemList.php
+++ Includes/Gamba/Loot/Item/ItemList.php
@@ -40,6 +40,89 @@ $itemList = [
         Whose blood will be spilt in the Bloodbath? Who will the victors be? How many will survive? Good luck...
         DESC 
     ],
+    [
+        'id' => $id++,
+        'name' => 'Rock',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        Maybe if you can throw is far enough...
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Throwing Dagger',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        Short dagger for throwing. It has no handguard.
+        The blade is polished, and its weight is expertly balanced.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Cinquedea',
+        'rarity' => Rarity::PURPLE->value,
+        'description' => <<<DESC
+        The design celebrates a beast's five fingers, symbolic of the intelligence once granted upon their kind.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Star-Lined Sword',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        Sword encrusted with a line of stars fashioned from small pieces of crude glintstone.
+        When bestowed with this weapon by their queen, the swordsmen swear to find the truth that lies at the end of the procession of stars.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Galatine',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        A sword this big cannot possibly be effective?
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Galatine Prime',
+        'rarity' => Rarity::GOLD->value,
+        'description' => <<<DESC
+        Like it's none prime counterpart... with a bit more gold.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Fragor',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        A large two-handed hammer, the Fragor requires great strength to wield, but impacts with enough force to send groups of enemies tumbling.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Stug',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        Firing a sticky, toxic, explosive compound, the Stug Gel Gun offers multiple ejection modes, delivering maximum damage in all situations.
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Brother Corhyn',
+        'rarity' => Rarity::BLUE->value,
+        'description' => <<<DESC
+        "Welcome to the Roundtable Hold. I'm Corhyn, a man of the cloth."
+        DESC 
+    ],
+    [
+        'id' => $id++,
+        'name' => 'Mask of the Quiet One',
+        'rarity' => Rarity::GOLD->value,
+        'description' => <<<DESC
+        "..."
+        DESC 
+    ],
 ];
 
+if(count($itemList) > 255) throw new Exception('Max item count is 255');
 return $itemList;
