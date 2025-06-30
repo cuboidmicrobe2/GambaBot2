96252d19ec87375c9825a4f1b6af33617479855a
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-06-30 21:54:57 +0200

    change

--- functions.php
+++ functions.php
@@ -5,5 +5,5 @@ namespace GambaBot;
 use Discord\Parts\Interactions\Interaction;
 
 function getUserId(Interaction $interaction) : string {
-    return isset($interaction->member) ? $interaction->member->user->id : $interaction->user->id;
+    return $interaction->member->user->id ?? $interaction->user->id;
 }
