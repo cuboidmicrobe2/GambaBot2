65bc5b6969b24d063546f7d1332a973b527d5cd9
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-07-03 20:42:38 +0200

    command interaction visual change

--- Commands/stats.php
+++ Commands/stats.php
@@ -11,13 +11,14 @@ global $discord, $gamba;
 
 $discord->listenCommand('stats', function(Interaction $interaction) use ($gamba, $discord) {
     $stats = $gamba->getUserStats(getUserId($interaction));
-
+    $goldMaxPity = GOLD_PITY_CAP;
+    $purpleMaxPity = PURPLE_PITY_CAP;
     $interaction->respondWithMessage(MessageBuilder::new()->addEmbed(new Embed($discord)
         ->setTitle('Stats')
         ->setDescription(<<<DESC
         **Coins** `{$stats['coins']}`
-        **Gold Pity** `{$stats['goldPity']}`
-        **Purple Pity** `{$stats['purplePity']}`
+        **Gold Pity** `{$stats['goldPity']}` out of {$goldMaxPity}
+        **Purple Pity** `{$stats['purplePity']}` out of {$purpleMaxPity}
         DESC)
         // ->addFieldValues('Coins', $stats['coins'] ?? 'error', true)
         // ->addFieldValues('Gold Pity', $stats['goldPity'] ?? 'error', true)
