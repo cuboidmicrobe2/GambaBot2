bfbafe8275f0ced8f6b85ec5fb9df3623dca224d
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-07-03 20:41:17 +0200

    new function to get an option value from the command interaction

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@ use Discord\Parts\Interactions\Interaction;
 function getUserId(Interaction $interaction) : string {
     return $interaction->member->user->id ?? $interaction->user->id;
 }
+
+function getOptionValue(string $offset, Interaction $interaction) : ?string {
+    if($interaction->data->options->offsetExists($offset)) return $interaction->data->options->offsetGet($offset)->value;
+    return null;
+}
+
+function isImplementing(object $object, string $interface) : bool {
+    return in_array($interface, class_implements($object));
+}
