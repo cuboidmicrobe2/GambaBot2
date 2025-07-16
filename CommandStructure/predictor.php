d845fc0cf2e6a0ca61706a275be695dbce9a7456
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-07-17 00:29:59 +0200

    added new command

---
+++ CommandStructure/predictor.php
@@ -0,0 +1,38 @@
+<?php
+
+use Discord\Builders\CommandBuilder;
+use Discord\Parts\Interactions\Command\Option;
+use Symfony\Component\Dotenv\Dotenv;
+use Discord\Discord;
+
+require_once './vendor/autoload.php';
+
+
+$dotenv = new Dotenv;
+$dotenv->load('./.env');
+
+
+$discord = new Discord([
+    'token' => $_ENV['DISCORD_TOKEN'],
+]);
+$discord->on('init', function(Discord $discord) {
+
+    //$discord->application->commands->delete('')->then(function() use ($discord) {
+        $discord->application->commands->save(
+            $discord->application->commands->create(CommandBuilder::new()
+                ->setName('predictor')
+                ->setDescription('guess the correct color and win exponential prices')
+
+                ->addOption(new Option($discord)
+                    ->setName('amount')
+                    ->setDescription('amount of coins to wager')
+                    ->setRequired(true)
+                    ->setType(Option::INTEGER)
+                    ->setMinValue(10)
+                )
+
+                ->toArray()
+            )
+        );
+    });
+//});
