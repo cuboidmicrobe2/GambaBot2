44581c9ef86c4241c325d490ed9ab67bc4e6a85d
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-07-17 00:29:12 +0200

    getCommandStrings() gives strings associated with given command interaction

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace GambaBot;
 
+use Debug\CMD_FONT_COLOR;
+use Debug\CMDOutput;
 use Discord\Parts\Interactions\Interaction;
 
 function getUserId(Interaction $interaction) : string {
@@ -17,4 +19,16 @@ function getOptionValue(string $offset, Interaction $interaction) : mixed {
 
 function isImplementing(object $object, string $interface) : bool {
     return in_array($interface, class_implements($object));
+}
+
+
+function getCommandStrings(Interaction $interaction) : ?array {
+    if(file_exists(__DIR__.'/Commands/content/strings.json')) {
+        $strings = json_decode(file_get_contents(__DIR__.'/Commands/content/strings.json'), true);
+        
+        return array_key_exists($interaction->data->name, $strings) ? $strings[$interaction->data->name] : null;
+    }
+    echo CMDOutput::new()->add('strings.json is missing from ' . __DIR__. '/content', CMD_FONT_COLOR::YELLOW), PHP_EOL;
+
+    return null;
 }
