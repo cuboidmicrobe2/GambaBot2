50b0d7e58c7a866e88f2c938c96685a5eb909360
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-07-31 23:43:30 +0200

    CardDeck v1

--- Includes/Gamba/CoinGame/Tools/PlayingCards/CardDeck.php
+++ Includes/Gamba/CoinGame/Tools/PlayingCards/CardDeck.php
@@ -7,30 +7,43 @@ namespace Gamba\CoinGame\Tools\PlayingCards;
 use ArrayAccess;
 use ArrayIterator;
 use Countable;
-use Gamba\CoinGame\Tools\PlayingCards\CardSuit;
-use Gamba\CoinGame\Tools\PlayingCards\CardFace;
-use Gamba\CoinGame\Tools\PlayingCards\Card;
 use IteratorAggregate;
+use ReflectionClass;
 use Traversable;
 
-final class CardDeck implements ArrayAccess, IteratorAggregate, Countable
+final class CardDeck implements ArrayAccess, Countable, IteratorAggregate
 {
-    public array $cards;
+    private array $cards;
+    private readonly array $deckBp;
 
     private int $cardIterator = 0;
 
-    public function __construct()
+    public function __construct(int $size = 52)
     {
-        foreach (CardSuit::cases() as $suit) {
-            foreach (CardFace::cases() as $face) {
-                $this->cards[] = new Card($suit, $face);
+        $addedCards = 0;
+        $tempDeck = [];
+        while ($addedCards < $size) {
+            foreach (CardSuit::cases() as $suit) {
+                foreach (CardFace::cases() as $face) {
+                    $this->cards[] = new Card($suit, $face);
+                }
             }
-        }
-    }
 
-    public static function fromCustomSize(int $size): self
-    {
-        
+            $this->shuffle();
+
+            foreach ($this->cards as $card) {
+                if ($addedCards >= $size) {
+                    continue;
+                }
+                
+                $tempDeck[] = $card;
+                $addedCards++;
+            }
+
+            $this->cards = [];
+        }
+        $this->deckBp = $tempDeck;
+        $this->resetDeck();
     }
 
     public function shuffle(): void
@@ -38,15 +51,44 @@ final class CardDeck implements ArrayAccess, IteratorAggregate, Countable
         shuffle($this->cards);
     }
 
+    /**
+     * Pick a **Card** form the deck and remove it from the deck.
+     * 
+     * @return null|Card    Returns a **Card** or null if **CardDeck** is empty
+     */
+    // #[NoDiscard]
+    public function pickCard(): ?Card
+    {
+        if (empty($this->cards)) { 
+            return null;
+        }
+
+        $key = array_key_first($this->cards);
+        $card = $this->cards[$key];
+        unset($this->cards[$key]);
+
+        return $card;
+    }
+
     public function nextCard(bool $shuffleOnEnd = false): Card
     {
-        if(!isset($this->cards[$this->cardIterator])) {
+        if (! isset($this->cards[$this->cardIterator])) {
             $this->cardIterator = 0;
+
+            if ($shuffleOnEnd) {
+                $this->shuffle();
+            }
         }
 
         $card = $this->cards[$this->cardIterator];
         $this->cardIterator++;
+
         return $card;
+    }
+
+    public function resetDeck(): void
+    {
+        $this->cards = $this->deckBp;
     }
 
     public function offsetExists(mixed $offset): bool
@@ -78,4 +120,6 @@ final class CardDeck implements ArrayAccess, IteratorAggregate, Countable
     {
         return count($this->cards);
     }
+
+
 }
