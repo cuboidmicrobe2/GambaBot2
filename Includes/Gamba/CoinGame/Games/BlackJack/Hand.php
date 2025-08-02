e0e7f5ca59435641000a22f7eb992c3bbdee5479
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-08-02 18:45:18 +0200

    Hand::getValue() now accounts for aces

--- Includes/Gamba/CoinGame/Games/BlackJack/Hand.php
+++ Includes/Gamba/CoinGame/Games/BlackJack/Hand.php
@@ -9,14 +9,21 @@ use Exception;
 use Gamba\CoinGame\Tools\PlayingCards\Card;
 use Gamba\CoinGame\Tools\PlayingCards\CardCollection;
 use Gamba\CoinGame\Tools\PlayingCards\CardFace;
+use Stringable;
 
-final class Hand implements Countable
+final class Hand implements Countable, Stringable
 {
+    private const int ACE = 710;
+
+    private const int ACE_MAX = 11;
+
+    private const int ACE_MIN = 1;
+
     public private(set) bool $playable = true;
 
     public private(set) CardCollection $cards;
-    
-    public function __construct(private bool $dealer = false)
+
+    public function __construct(private readonly bool $dealer = false)
     {
         $this->cards = new CardCollection(11);
     }
@@ -25,8 +32,16 @@ final class Hand implements Countable
     {
         $string = '';
 
-        /** @var Card $card */
-        foreach ($this->cards as $card) {
+        /**
+         * @var int $key
+         * @var Card $card
+         */
+        foreach ($this->cards as $key => $card) {
+            if ($this->dealer && $key === 0) {
+                $string .= '?? ';
+                continue;
+            }
+
             $string .= $card->asString(emoji: true).' ';
         }
 
@@ -45,13 +60,28 @@ final class Hand implements Countable
     public function getValue(): int
     {
         $value = 0;
+        $aceCount = 0;
         foreach ($this->cards as $card) {
-            $value += $this->getCardValue($card);
+            $cardValue = $this->getCardValue($card);
+            if ($cardValue === self::ACE) {
+                $aceCount++;
+
+                continue;
+            }
+            $value += $cardValue;
+        }
+
+        for ($i = 0; $i < $aceCount; $i++) {
+            if (($value + self::ACE_MAX) > 21) {
+                $value += self::ACE_MIN;
+            } else {
+                $value += self::ACE_MAX;
+            }
         }
 
         return $value;
     }
-    
+
     public function lock(): void
     {
         $this->playable = false;
@@ -65,7 +95,7 @@ final class Hand implements Countable
     private function getCardValue(Card $card): int
     {
         return match ($card->face) {
-            CardFace::ACE => 11,
+            CardFace::ACE => self::ACE,
             CardFace::TWO => 2,
             CardFace::THREE => 3,
             CardFace::FOUR => 4,
