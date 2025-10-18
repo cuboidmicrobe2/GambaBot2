c7598f1e248624b324877667332a1c0b5f94e5a6
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-10-18 23:32:41 +0200

    New Exception

---
+++ Includes/Infrastructure/Exceptions/UndefinedProperty.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Exceptions;
+
+use Exception;
+use Throwable;
+
+class UndefinedProperty extends Exception {
+    public function __construct(string $message = "", int $code = 0, Throwable|null $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
