4171ad96281d4345a8db547d5e26184ce7af95fa
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-11-25 01:36:58 +0100

    Change SplObjectStorage to array and add start custom attribute suport

--- Includes/Gamba/CoinGame/GameHandler.php
+++ Includes/Gamba/CoinGame/GameHandler.php
@@ -5,15 +5,17 @@ declare(strict_types=1);
 namespace Gamba\CoinGame;
 
 use Debug\Debug;
+use Debug\MessageType;
 use Deprecated;
 use Discord\Builders\Components\ActionRow;
 use Gamba\CoinGame\MultiInteractionLink;
+use Gamba\CoinGame\Attributes\LogOnClose;
 use Exception;
 use InvalidArgumentException;
-use SplObjectStorage;
 use TimedGameInstance;
 use WeakMap;
 
+use function GambaBot\Tools\getAttributes;
 use function GambaBot\Tools\isUsing;
 
 /**
@@ -32,7 +34,12 @@ final class GameHandler
         }
     }
 
-    private SplObjectStorage $games;
+    // private SplObjectStorage $games;
+
+    /**
+     * @var array<string, GameInstance>
+     */
+    private array $games = [];
 
     /**
      * @var WeakMap<GameInstance, GameData|GameDataMap>
@@ -41,19 +48,21 @@ final class GameHandler
 
     public function __construct()
     {
-        $this->games = new SplObjectStorage;
         $this->gameData = new WeakMap; // data is stored in a WeakMap beacuse SplObjectSotrage uses SeekableIterator and prob wont work async but is needed to prevent obj from gc
     }
 
-    public function addGame(GameInstance &$game, GameData|GameDataMap $data): void
+    /**
+     * Add a game to the **GameHandler**.
+     * 
+     * @param GameInstance $game The game to add.
+     * @param GameData|GameDataMap $data Data associated with the game.
+     */
+    public function addGame(GameInstance $game, GameData|GameDataMap $data): void
     {
 
         if ($this->IdExists($data->id)) {
             throw new Exception('a GameInstance with the id '.$data->id.' already exists');
         }
-        // if ($this->userIsPlaying($data->owner, $game::class)) {
-        //     throw new Exception('the user is already playing that game');
-        // }
 
         if (! $data instanceof GameDataMap && isUsing($game, MultiInteractionLink::class)) {
             throw new InvalidArgumentException('Games with a MultiInteractionLink must pass a GameDataMap');
@@ -63,42 +72,36 @@ final class GameHandler
             throw new InvalidArgumentException('Games without a MultiInteractionLink must not use a GameDataMap');
         }
 
-        $this->games->attach($game);
+        $this->games[$data->id] = $game;
         $data->setType($game::class);
         $this->gameData[$game] = $data;
     }
 
-    // /**
-    //  * @param GameInstance<MultiInteractionLink> $game
-    //  */
-    // public function addLinkedGame(GameInstance &$game, array $data): void
-    // {
-    //     if (! isUsing($game, MultiInteractionLink::class)) {
-    //         throw new InvalidArgumentException($game::class.' does not use the MultiInteractionLink trait');
-    //     }
-    // }
-
     public function closeGame(GameInstance|string $game): bool
     {
-        if (! $game instanceof GameInstance) {
-            $game = $this->getFromId($game);
-            if (!$game instanceof GameInstance) {
-                return false;
-            } // something...
+        $gameId = ($game instanceof GameInstance) ? $this->gameData[$game]->id : $game;
+
+        if (isset($this->games[$gameId])) {
+            echo self::createUpdateMessage('', 'removed game '.json_encode($this->gameData[$game])), PHP_EOL;
+
+            $gameInstance = $this->games[$gameId];
+            $attrList = getAttributes($gameInstance);
+            if (isset($attrList[LogOnClose::class])) {
+                $attrList[LogOnClose::class]->newInstance()->log();
+            }
+
+            unset($this->games[$gameId]);
+            return true;
         }
-
-        echo self::createUpdateMessage('', 'removed game '.json_encode($this->gameData[$game])), PHP_EOL;
-
-        $this->games->detach($game);
-        unset($game);
-
-        return true;
+        
+        echo self::createUpdateMessage('', 'could not find game: '.$gameId, MessageType::WARNING), PHP_EOL;
+        return false;
     }
 
-    // #[NoDiscard]
+    #[\NoDiscard]
     public function getGame(string $interactionId): ?GameInstance
     {
-        return $this->getFromId($interactionId);
+        return $this->games[$interactionId] ?? null;
     }
 
     /**
@@ -112,7 +115,7 @@ final class GameHandler
         return $this->getFromId($gameId[0]);
     }
 
-    // #[NoDiscard]
+    #[\NoDiscard]
     public function getGameData(GameInstance $game, ?string $interactionId = null): GameData
     {
         $data = $this->gameData[$game] ?? null;
@@ -141,7 +144,7 @@ final class GameHandler
 
     // }
 
-    // #[NoDiscard]
+    #[\NoDiscard]
     public function getNewActionRow(GameInstance $game): ActionRow
     {
         $row = new ActionRow;
@@ -152,35 +155,6 @@ final class GameHandler
 
         return $row;
     }
-
-    // /**
-    //  *  Get GameInstance|false or bool if user is playing a specific or any game
-    //  */
-    // public function userIsPlaying(string $uid, ?string $game = null, bool $returnGameInstance = false): GameInstance|bool
-    // {
-
-    //     if ($returnGameInstance && $game === null) {
-    //         throw new Exception('must specify game to return GameInstance');
-    //     }
-
-    //     if ($game === null) {
-    //         foreach ($this->gameData as $data) {
-    //             if ($data->owner === $uid) {
-    //                 return true;
-    //             }
-    //         }
-
-    //         return false;
-    //     }
-
-    //     foreach ($this->gameData as $game => $data) {
-    //         if ($data->owner === $uid && $game === $data->gameType) {
-    //             return $returnGameInstance ? $game : true;
-    //         }
-    //     }
-
-    //     return false;
-    // }
 
     /**
      * Handle timed events
@@ -195,12 +169,15 @@ final class GameHandler
 
             if ($game->expired()) {
                 echo self::createUpdateMessage('', 'removed old game '.json_encode($data)), PHP_EOL;
-                $this->games->detach($game);
-                unset($game);
+                unset($this->games[$data->id]);
             }
         }
     }
 
+    /**
+     * @deprecated use GameHandler::getGame()
+     */
+    #[\Deprecated('use GameHandler::getGame()')]
     private function getFromId(string $id): ?GameInstance
     {
         foreach ($this->gameData as $game => $data) {
