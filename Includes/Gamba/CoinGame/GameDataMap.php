92c596c86c90cd32b33c8f732e9b670a2e8561f1
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-11-25 01:34:44 +0100

    Add $id property

--- Includes/Gamba/CoinGame/GameDataMap.php
+++ Includes/Gamba/CoinGame/GameDataMap.php
@@ -9,15 +9,29 @@ use InvalidArgumentException;
 final class GameDataMap
 {
     /**
+     * Id of first added **GameData** object.
+     */
+    public readonly string $id;
+
+    /**
      * @var array<string, GameData>
      */
     private array $data = [];
 
     public function add(GameData $data): void
     {
+        if(! isset($this->id)) {
+            $this->id = $data->id;
+        }
         $this->data[$data->id] = $data;
     }
 
+    /**
+     * Get a **GameData** object linked to an id. 
+     *
+     * @param string $id Interaction id.
+     * @throws InvalidArgumentException If the id does not exist.
+     */
     public function get(string $id): GameData
     {
         if (! isset($this->data[$id])) {
