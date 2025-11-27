853083f50a8467a9184dda3049a20d3ff88c9610
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2025-11-27 01:11:02 +0100

    Run rector + pint

--- Includes/Tools/Discord/Text/MentionTypeInterface.php
+++ Includes/Tools/Discord/Text/MentionTypeInterface.php
@@ -7,6 +7,8 @@ namespace Tools\Discord\Text;
 interface MentionTypeInterface
 {
     public function user(string|int $userId): string;
+
     public function channel(string|int $channelId): string;
+
     public function command(string $commandName, string|int $commandId): string;
 }
